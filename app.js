6f1e831104948c95eb491c8cfaeb2809dd99d0f2
raymondsubianto <subiantoraymond20@gmail.com>
2023-05-26 14:44:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv'
 import { database } from './config/connection.js'
 import ayoPintar from './routes/app.js'
 import User from './models/user.js'
+import Soal from './models/soal.js'
+import Nilai from './models/nilai.js'
+import Mata_Pelajaran from './models/mata_pelajaran.js'
 
 dotenv.config()
 database()
@@ -10,11 +13,14 @@ database()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-try {
-    await User.sync({ force: true })
-} catch (error) {
-    console.log(`error : ${error.message}`)
-}
+// try {
+//     await User.sync({ force: true })
+//     await Nilai.sync({ force: true })
+//     await Soal.sync({ force: true })
+//     await Mata_Pelajaran.sync({ force: true })
+// } catch (error) {
+//     console.log(`error : ${error.message}`)
+// }
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
